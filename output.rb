26fe07220e215c174d27560f513bcc8eb5008236
Michal Opala <mopala@opennebula.io>
2021-12-24 13:00:02 +0100

    stdout

--- output.rb
+++ output.rb
@@ -3,9 +3,10 @@ require 'json'
 begin
     name = "asd"
 
-    outputs = JSON.parse %x{ terraform output -json -no-color }
-    puts outputs
+    stdout = %x{ terraform output -json -no-color }
+    puts stdout
 
+    outputs = JSON.parse stdout
     output1 = outputs['output1_' << name]['value']
     output2 = outputs['output2_' << name]['value']
     puts output1, output2
