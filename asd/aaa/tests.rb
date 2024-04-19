c0979c851a7dd651cd51c58a6aba9e83fad56afd
Michal Opala <mopala@opennebula.io>
2024-04-19 04:13:25 +0200

    ASD

---
+++ asd/aaa/tests.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.describe 'aaa' do
+    it 'aaa' do
+        expect('aaa').to eq 'aaa'
+    end
+end
---
+++ asd/bbb/tests.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.describe 'bbb' do
+    it 'bbb' do
+        expect('bbb').to eq 'bbb'
+    end
+end
